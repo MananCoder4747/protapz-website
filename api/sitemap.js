ea83a0b3c8c67001284989b2852fdd5dc36a8fd3
Manan Khunt <manank4747@gmail.com>
2026-01-24 14:55:23 +0530

    Update: latest edits

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,4 +1,7 @@
 export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  res.setHeader('Cache-Control', 'public, max-age=3600');
+  
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
@@ -27,7 +30,5 @@ export default function handler(req, res) {
   </url>
 </urlset>`;
 
-  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
-  res.setHeader('Cache-Control', 'public, max-age=3600, s-maxage=3600');
   res.status(200).send(sitemap);
 }

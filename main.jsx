172f7a10315b92acfdf2edcd48932186e7bb2add
Manan Khunt <manank4747@gmail.com>
2026-01-25 09:02:16 +0530

    Add Vercel Speed Insights and clean node_modules

--- main.jsx
+++ main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './src/index.css'
 import App from './App.jsx'
 import './styles.css'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 )
